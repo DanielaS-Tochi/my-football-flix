5059a97b25cd0ff6421e19ee4faa9277dc46549c
DanielaSilvanaTochi <133720661+DanielaS-Tochi@users.noreply.github.com>
2025-01-14 00:01:35 +0000

    Added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { PiSoccerBallFill } from "react-icons/pi";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
       
       <h1>My Football Flix</h1>
       <div className="card">
+      
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
